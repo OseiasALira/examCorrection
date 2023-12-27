28e78e10f77e61742a0986dd7bf20aeed0fecf9e
Oséias A. Lira <oseias.a.lira@gmail.com>
2023-12-27 17:19:25 -0300

    Create window with student registration form

---
+++ src/main/java/com/oseias/examcorrection/controller/AddStudentController.java
@@ -0,0 +1,24 @@
+package com.oseias.examcorrection.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+
+    @FXML
+    private Button cancelNewStudentButton;
+
+    @FXML
+    private TextField firstNameStudentTextField;
+
+    @FXML
+    private TextField lastNameStudentTextField;
+
+    @FXML
+    private TextField parentNameStudentTextField;
+
+    @FXML
+    private Button saveNewStudentButton;
+
+}

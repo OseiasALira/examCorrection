e583bd86fd1d4cf7aca2e528de05a591aef50e59
Oséias A. Lira <oseias.a.lira@gmail.com>
2023-12-27 17:42:11 -0300

    Create window for new exam registration

---
+++ src/main/java/com/oseias/examcorrection/controller/AddExamController.java
@@ -0,0 +1,21 @@
+package com.oseias.examcorrection.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class AddExamController {
+
+    @FXML
+    private Button cancelNewExamButton;
+
+    @FXML
+    private ChoiceBox<?> examStyleChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> quantityOfQuestionsChoiceBox;
+
+    @FXML
+    private Button saveNewExamButton;
+
+}
